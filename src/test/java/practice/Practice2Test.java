830fc4e82b6e27f6c85da2b47ccf168628b1b351
anil <anil@LAPTOP-VN3EE17F>
2022-09-23 02:31:16 +0530

    first commit

---
+++ src/test/java/practice/Practice2Test.java
@@ -0,0 +1,10 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class Practice2Test {
+	@Test
+public void practice() {
+	System.out.println("helo");
+}
+}
---
+++ src/test/java/practice/practices1Test.java
@@ -0,0 +1,11 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class practices1Test {
+	@Test
+public void practices1() {
+		
+	System.out.println("hiii");
+}
+}
