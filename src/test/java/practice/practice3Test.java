76886358b5b346977de32d222d32389730b2b83b
anil <anil@LAPTOP-VN3EE17F>
2022-09-23 11:32:41 +0530

    get commit

---
+++ src/test/java/practice/practice3Test.java
@@ -0,0 +1,13 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class practice3Test {
+	@Test
+public void practice3() {
+	String Browser = System.getProperty("br");
+	String Url = System.getProperty("ur");
+	System.out.println(Browser);
+	System.out.println(Url);
+}
+}
